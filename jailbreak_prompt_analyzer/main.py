44157e43c7b81c3bc601f958b589a87b4270d36b
anusures <anusures@cisco.com>
2025-04-09 17:17:23 -0700

    Include comments to the code

--- jailbreak_prompt_analyzer/main.py
+++ jailbreak_prompt_analyzer/main.py
@@ -1,6 +1,9 @@
 # Copyright AGNTCY Contributors (https://github.com/agntcy)
 # SPDX-License-Identifier: Apache-2.0
 import json
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(dotenv_path=find_dotenv(usecwd=True))
 from jailbreak_prompt_analyzer.jailbreak_prompt_analyzer import (
     graph,
     AgentState,
@@ -10,7 +13,7 @@ from jailbreak_prompt_analyzer.state import Message, Type as MsgType
 
 
 def main():
-    # TODO: Read the variables from the sentinel agent
+    # TODO: Read the variables from the sentinel agent and currently getting the inputs from the user terminal
     #unfiltered_llm_response = " "
     #intention_analyzer_output = " "
     unfiltered_llm_response = input("Please Enter the LLM response >>>")
@@ -19,7 +22,7 @@ def main():
                                        intention_analyzer_output=intention_analyzer_output)
     out = graph.invoke(prompt_analyzer_input, {"configurable": {"thread_id": "foo"}})
     output: OutputState = OutputState.model_validate(out)
-    print("The List of prompts that can produce similar LLM response : ")
+    print("The List of prompts that can produce similar LLM response : \n ")
     print(output.prompt_analyzer_output)
 
 
